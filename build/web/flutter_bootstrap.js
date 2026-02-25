6c4faf70c66b1b0abb41c1e9454dd3168eae1633
RonaldoDev07 <ronaldotecinfor2024@gmail.com>
2026-02-24 23:26:04 -0500

    feat: remover todos os arquivos de gestão financeira

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1060573209"
+    serviceWorkerVersion: "552005407"
   }
 });
