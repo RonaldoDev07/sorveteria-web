ee75c4f4744b76f9e3d554a1cdb511c7e6c949d4
RonaldoDev07 <ronaldodev07@github.com>
2026-02-25 18:15:18 -0500

    build: adicionar arquivos de build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "icons/Icon-512.png": "5676a8a85df0593dfe7f613db5a9ab9e",
 "icons/Icon-maskable-192.png": "7e96679fc54546e22e211dd1e6272f1a",
 "icons/Icon-maskable-512.png": "5676a8a85df0593dfe7f613db5a9ab9e",
-"index.html": "67627a0ee8b2ad47a8a7c8645634015e",
-"/": "67627a0ee8b2ad47a8a7c8645634015e",
-"main.dart.js": "155951f743f7ca7ff246b598bc4eac81",
+"index.html": "23ec77c72bcf824d7fd907d1cfaa75c6",
+"/": "23ec77c72bcf824d7fd907d1cfaa75c6",
+"main.dart.js": "65c5097e83224060b0098cb8dbe91abd",
 "manifest.json": "37ba3984fa359dc2155d1061dfe87a3c",
 "upload.html": "dd48c80006989f46cb3564a5caceea03",
 "version.json": "2b10f146f2c4d861322fc0316b724c71"};
