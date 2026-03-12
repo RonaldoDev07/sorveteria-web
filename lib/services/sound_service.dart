d185be70b8def5a5806ce42f839f974ceff09f15
RonaldoDev07 <ronaldotecinfor2024@gmail.com>
2026-03-12 00:20:02 -0500

    v1.2.8

--- lib/services/sound_service.dart
+++ lib/services/sound_service.dart
@@ -1,7 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show AudioElement;
 
 class SoundService {
   static final AudioPlayer _player = AudioPlayer();
@@ -11,43 +9,10 @@ class SoundService {
   static const String _errorSound = 'sounds/error.mp3';
   static const String _clickSound = 'sounds/click.mp3';
   
-  // Players HTML5 para web (melhor compatibilidade)
-  static html.AudioElement? _webSuccessPlayer;
-  static html.AudioElement? _webErrorPlayer;
-  static html.AudioElement? _webClickPlayer;
-  static bool _webPlayersInitialized = false;
-  
-  /// Inicializa players HTML5 para web
-  static void _initWebPlayers() {
-    if (kIsWeb && !_webPlayersInitialized) {
-      try {
-        _webSuccessPlayer = html.AudioElement('assets/$_successSound');
-        _webSuccessPlayer!.load();
-        
-        _webErrorPlayer = html.AudioElement('assets/$_errorSound');
-        _webErrorPlayer!.load();
-        
-        _webClickPlayer = html.AudioElement('assets/$_clickSound');
-        _webClickPlayer!.load();
-        
-        _webPlayersInitialized = true;
-        print('✅ Players HTML5 inicializados para web');
-      } catch (e) {
-        print('Erro ao inicializar players web: $e');
-      }
-    }
-  }
-  
   /// Toca som de sucesso (venda concluída, cadastro salvo, etc)
   static Future<void> playSuccess() async {
     try {
-      if (kIsWeb) {
-        _initWebPlayers();
-        _webSuccessPlayer?.currentTime = 0;
-        await _webSuccessPlayer?.play();
-      } else {
-        await _player.play(AssetSource(_successSound));
-      }
+      await _player.play(AssetSource(_successSound));
     } catch (e) {
       print('Erro ao tocar som de sucesso: $e');
     }
@@ -56,13 +21,7 @@ class SoundService {
   /// Toca som de erro (falha na operação, validação, etc)
   static Future<void> playError() async {
     try {
-      if (kIsWeb) {
-        _initWebPlayers();
-        _webErrorPlayer?.currentTime = 0;
-        await _webErrorPlayer?.play();
-      } else {
-        await _player.play(AssetSource(_errorSound));
-      }
+      await _player.play(AssetSource(_errorSound));
     } catch (e) {
       print('Erro ao tocar som de erro: $e');
     }
@@ -71,13 +30,7 @@ class SoundService {
   /// Toca som de click (botões, ações rápidas)
   static Future<void> playClick() async {
     try {
-      if (kIsWeb) {
-        _initWebPlayers();
-        _webClickPlayer?.currentTime = 0;
-        await _webClickPlayer?.play();
-      } else {
-        await _player.play(AssetSource(_clickSound));
-      }
+      await _player.play(AssetSource(_clickSound));
     } catch (e) {
       print('Erro ao tocar som de click: $e');
     }
@@ -86,13 +39,7 @@ class SoundService {
   /// Para o som atual
   static Future<void> stop() async {
     try {
-      if (kIsWeb) {
-        _webSuccessPlayer?.pause();
-        _webErrorPlayer?.pause();
-        _webClickPlayer?.pause();
-      } else {
-        await _player.stop();
-      }
+      await _player.stop();
     } catch (e) {
       print('Erro ao parar som: $e');
     }
@@ -101,17 +48,7 @@ class SoundService {
   /// Libera recursos
   static Future<void> dispose() async {
     try {
-      if (kIsWeb) {
-        _webSuccessPlayer?.pause();
-        _webErrorPlayer?.pause();
-        _webClickPlayer?.pause();
-        _webSuccessPlayer = null;
-        _webErrorPlayer = null;
-        _webClickPlayer = null;
-        _webPlayersInitialized = false;
-      } else {
-        await _player.dispose();
-      }
+      await _player.dispose();
     } catch (e) {
       print('Erro ao liberar recursos de áudio: $e');
     }
