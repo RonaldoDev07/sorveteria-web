e4fe2b658f097ad2a2844e1a8d497d4be52c717c
RonaldoDev07 <ronaldotecinfor2024@gmail.com>
2026-03-11 21:25:27 -0500

    build: atualizar web com sons em todas telas

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e3d73f537e0df86be173a0aed1f7b752
 "favicon.svg": "ef9ed0cba87fabcd51dd4be8b09e7478",
 "favicon_generator.html": "380daa50f4342fc201e625265efb7124",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "7c7413c8807dd51678006de585fe46b7",
+"flutter_bootstrap.js": "336b23d5204342d97ccb873ea702c499",
 "icon.html": "9b752494f9506f201c73f4aa51f91166",
 "icons/Icon-192.png": "7e96679fc54546e22e211dd1e6272f1a",
 "icons/Icon-512.png": "5676a8a85df0593dfe7f613db5a9ab9e",
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e3d73f537e0df86be173a0aed1f7b752
 "icons/Icon-maskable-512.png": "5676a8a85df0593dfe7f613db5a9ab9e",
 "index.html": "40fb0593d8f3d2f217677acda30ae967",
 "/": "40fb0593d8f3d2f217677acda30ae967",
-"main.dart.js": "900169cdb87ded8d710bd1738e0c7acd",
+"main.dart.js": "200b96438d63da9b80af0a077b24c25f",
 "manifest.json": "37ba3984fa359dc2155d1061dfe87a3c",
 "upload.html": "dd48c80006989f46cb3564a5caceea03",
 "version.json": "2ec2156c24d9f489e555e4355a5f0999"};
