d7de7bd6a425b21f637f33a8fb46be88696d273c
RonaldoDev07 <ronaldotecinfor2024@gmail.com>
2026-03-13 22:17:07 -0500

    build: atualizar build web com historico detalhado compras

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1605592594"
+    serviceWorkerVersion: "320262740"
   }
 });
